834df958cbd0677b7f927f2f7e0bbc0895ce0861
Abdullah Said <114074696+Abdullah-MLE@users.noreply.github.com>
2024-12-15 16:16:59 +0200

    modified:   1startingcode.php 	modified:   aboutUsPage.php 	renamed:    login.css -> css/login.css 	renamed:    style.css -> css/style.css 	modified:   loginPage.php 	renamed:    1.jpg -> photos/1.jpg 	renamed:    user img.jpeg -> photos/user img.jpeg

--- 1startingcode.php
+++ 1startingcode.php
@@ -2,144 +2,156 @@
     // Start the session
     session_start();
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<title>Tasks Management</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" href="style.css">
-</head>
-<body>
+    <head>
+        <title>Tasks Management</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
 
-    <!-- DB CONNECTION -->
-    <?php
-    // Database connection setup
-    $servername = "localhost";
-    $username = "root";
-    $dbpassword = "";
-    $dbname = "website_project";
+        <!-- DB CONNECTION -->
+        <?php
+            // Database connection setup
+            $servername = "localhost";
+            $username = "root";
+            $dbpassword = "";
+            $dbname = "website_project";
 
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $dbpassword, $dbname);
+            // Create connection
+            $conn = mysqli_connect($servername, $username, $dbpassword, $dbname);
 
-    // Check connection
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
+            // Check connection
+            if (!$conn) {
+                die("Connection failed: " . mysqli_connect_error());
+            }
 
-    $id = $_SESSION['id'];
-    $full_name = $_SESSION['full_name'];
-    $username = $_SESSION['username'];
-    $password = $_SESSION['password'];
-    $role = $_SESSION['role'];
-    $email = $_SESSION['email'];
-    $phone_number = $_SESSION['phone_number'];
-    $profile_picture = $_SESSION['profile_picture'];
-    $address = $_SESSION['address'];
-    $created_at = $_SESSION['hire_date'];
-    ?>
-    
+            $id = $_SESSION['id'];
+            $full_name = $_SESSION['full_name'];
+            $username = $_SESSION['username'];
+            $password = $_SESSION['password'];
+            $role = $_SESSION['role'];
+            $email = $_SESSION['email'];
+            $phone_number = $_SESSION['phone_number'];
+            $profile_picture = $_SESSION['profile_picture'];
+            $address = $_SESSION['address'];
+            $created_at = $_SESSION['hire_date'];
+        ?>
         <!-- Navigation Bar -->
-    
-    <header class="header">
-		<h2 class="u-name">Tasks <b>Management</b></h2>
-		<h3 class="u-name">@<b> <?php echo $username ?></b></h3>
-	</header>
-    <div class="body">
-		<nav class="side-bar">
-			<div class="user-p">
-				<img src="1.jpg">
+        <header class="header">
+            <h2 class="u-name">Tasks <b>Management</b></h2>
+            <h3 class="u-name">@<b> <?php echo $username ?></b></h3>
+        </header>
+        <!-- Page Body -->
+        <div class="body">
+            <!-- side bar -->
+            <nav class="side-bar">
+                <div class="user-p">
+                    <img src="1.jpg">
+                </div>
+                <!--Employee Navigation Bar-->
+                <?php if ($role === 'admin'): ?>
+                    <ul>
+                        <li>
+                            <a href="DashboardPage.php">
+                                <i class="fa fa-tachometer" aria-hidden="true"></i>
+                                <span>Dashboard</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="tasksPage.php">
+                                <i class="fa fa-bell" aria-hidden="true"></i>
+                                <span>Tasks</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="createTasksPage.php">
+                                <i class="fa fa-tasks" aria-hidden="true"></i>
+                                <span>Create Task</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="ManageUsersPage.php">
+                                <i class="fa fa-users" aria-hidden="true"></i>
+                                <span>Users</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="adduserpage.php">
+                                <i class="fa fa-check-square" aria-hidden="true"></i>
+                                <span>Create Users</span>
+                            </a>
+                        </li>
 
-			</div>
-			<!--Employee Navigation Bar-->
-            <?php if ($role === 'admin'): ?>
-                <ul>
-                    <li>
-                        <a href="DashboardPage.php">
-                            <i class="fa fa-tachometer" aria-hidden="true"></i>
-                            <span>Dashboard</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="tasksPage.php">
-                            <i class="fa fa-bell" aria-hidden="true"></i>
-                            <span>Tasks</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="createTasksPage.php">
-                            <i class="fa fa-tasks" aria-hidden="true"></i>
-                            <span>Create Task</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="ManageUsersPage.php">
-                            <i class="fa fa-users" aria-hidden="true"></i>
-                            <span>Users</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="adduserpage.php">
-                            <i class="fa fa-check-square" aria-hidden="true"></i>
-                            <span>Create Users</span>
-                        </a>
-                    </li>
+                        <li>
+                            <a href="profilePage.php">
+                                <i class="fa fa-user" aria-hidden="true"></i>
+                                <span>Profile</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="aboutUsPage.php">
+                                <i class="fa fa-info-circle" aria-hidden="true"></i>
+                                <span>About Us</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="loginpage.php">
+                                <i class="fa fa-power-off" aria-hidden="true"></i>
+                                <span>Logout</span>
+                            </a>
+                        </li>
+                    </ul>
+                <?php else: ?>
+                    <ul>
+                        <li>
+                            <a href="DashboardPage.php">
+                                <i class="fa fa-tachometer" aria-hidden="true"></i>
+                                <span>Dashboard</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="tasksPage.php">
+                                <i class="fa fa-bell" aria-hidden="true"></i>
+                                <span>My Tasks</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="profilePage.php">
+                                <i class="fa fa-user" aria-hidden="true"></i>
+                                <span>Profile</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="aboutUsPage.php">
+                                <i class="fa fa-info-circle" aria-hidden="true"></i>
+                                <span>About Us</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="loginpage.php">
+                                <i class="fa fa-power-off" aria-hidden="true"></i>
+                                <span>Logout</span>
+                            </a>
+                        </li>
+                    </ul>
+                <?php endif; ?>
 
-                    <li>
-                        <a href="profilePage.php">
-                            <i class="fa fa-user" aria-hidden="true"></i>
-                            <span>Profile</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="aboutUsPage.php">
-                            <i class="fa fa-info-circle" aria-hidden="true"></i>
-                            <span>About Us</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="loginpage.php">
-                            <i class="fa fa-power-off" aria-hidden="true"></i>
-                            <span>Logout</span>
-                        </a>
-                    </li>
+            </nav>
+            <!-- main content -->
+            <section class="section-1">
+                <h1 id="header">Student Names</h1>
+                <ul id="names-list">   
+                    <li class="name-item">Abdelrhman Hany</li>
+                    <li class="name-item">Mohammed Ashraf</li>
+                    <li class="name-item">Abdallah Said</li>
+                    <li class="name-item">Rofyda Khaled</li>
+                    <li class="name-item">Hager Ehab</li>
+                    <li class="name-item">Reem Ashraf</li>
                 </ul>
-            <?php else: ?>
-                <ul>
-                    <li>
-                        <a href="DashboardPage.php">
-                            <i class="fa fa-tachometer" aria-hidden="true"></i>
-                            <span>Dashboard</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="tasksPage.php">
-                            <i class="fa fa-bell" aria-hidden="true"></i>
-                            <span>My Tasks</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="profilePage.php">
-                            <i class="fa fa-user" aria-hidden="true"></i>
-                            <span>Profile</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="aboutUsPage.php">
-                            <i class="fa fa-info-circle" aria-hidden="true"></i>
-                            <span>About Us</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="loginpage.php">
-                            <i class="fa fa-power-off" aria-hidden="true"></i>
-                            <span>Logout</span>
-                        </a>
-                    </li>
-                </ul>
-            <?php endif; ?>
-
-		</nav>
-		<section class="section-1">
+            </section>
+        </div>
+    </body>
+</html>
 
--- loginPage.php
+++ loginPage.php
@@ -2,32 +2,11 @@
     // Start the session
     session_start();
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
-    <h1>Login</h1>
-    <form action="loginPage.php" method="post">
-        <label>Username:</label>
-        <input type="text" name="username" required><br><br>
-
-        <label>Password:</label>
-        <input type="password" name="password" required><br><br>
-
-        <input type="submit" name="login" value="login">
-    </form>
-
-    <?php
+<?php
     // Database connection setup
     $servername = "localhost";
     $username = "root";
     $dbpassword = "";
-    // $port = 3308;
     $dbname = "website_project";
 
     // Create connection
@@ -36,40 +15,62 @@
     // Check connection
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
-    }
+    }?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login page </title> 
+        <link rel="stylesheet" type="text/css" href="css/login.css">   
+    </head>
+    <body class="bg">
+        <div class="start">
+            <form method="post">
+                <center>
+                    <h1>login page</h1>
+                    <img src="user img.jpeg" class="img"/>
+                </center>
 
-    // Handle form submission
-    if (isset($_POST["login"])) {
-
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-
-        $result = mysqli_query($conn, "SELECT * FROM users WHERE username='$username' AND password='$password'");
-
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-            
-            $_SESSION['id'] = $row['id'];
-            $_SESSION['full_name'] = $row['full_name'];
-            $_SESSION['username'] = $row['username'];
-            $_SESSION['password'] = $row['password'];
-            $_SESSION['role'] = $row['role'];
-            $_SESSION['email'] = $row['email'];
-            $_SESSION['phone_number'] = $row['phone_number'];
-            $_SESSION['profile_picture'] = $row['profile_picture'];
-            $_SESSION['address'] = $row['address'];
-            $_SESSION['hire_date'] = $row['hire_date'];
-
-            header("Location: dashboardPage.php");
-        } else {
-            echo "Invalid username or password.";
-        }
-    }
-
-    // Close the connection
-    mysqli_close($conn);
-    ?>
-            </section>
+                <input type="text" name="username"class="txt" placeholder="enter username" required /><br/><br/>
+                <input type="password" name="password"class="txt" placeholder="enter password" required/><br/><br/>
+                <input type="submit" name="login" class="btn" value="login"></input><br/>
+            </form>
         </div>
+
+        <?php
+
+            // Handle form submission
+            if (isset($_POST["login"])) {
+
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+
+                $result = mysqli_query($conn, "SELECT * FROM users WHERE username='$username' AND password='$password'");
+
+                if ($result->num_rows > 0) {
+                    $row = $result->fetch_assoc();
+                    
+                    $_SESSION['id'] = $row['id'];
+                    $_SESSION['full_name'] = $row['full_name'];
+                    $_SESSION['username'] = $row['username'];
+                    $_SESSION['password'] = $row['password'];
+                    $_SESSION['role'] = $row['role'];
+                    $_SESSION['email'] = $row['email'];
+                    $_SESSION['phone_number'] = $row['phone_number'];
+                    $_SESSION['profile_picture'] = $row['profile_picture'];
+                    $_SESSION['address'] = $row['address'];
+                    $_SESSION['hire_date'] = $row['hire_date'];
+
+                    header("Location: dashboardPage.php");
+                } else {
+                    echo "Invalid username or password.";
+                }
+            }
+
+            // Close the connection
+            mysqli_close($conn);
+        ?>
     </body>
 </html>
+
